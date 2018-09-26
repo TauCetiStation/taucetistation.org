b150ccda7b0ce8a7896734c7cdbd7e8b31d83550
volas <volas@ya.ru>
2018-09-26 23:56:36 +0300

    Byond-redirect pages

--- src/routes/server.php
+++ src/routes/server.php
@@ -3,7 +3,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-//todo: роуты для html-редиректов
 $app->get('/server/{name}[/{format:json}]', function ($request, $response, $args) {
 	$settings = $this->get('settings')['servers'];
 
@@ -24,4 +23,20 @@ $app->get('/server/{name}[/{format:json}]', function ($request, $response, $args
 		$args['data'] = $data;
 		return $this->view->render($response, 'server.html', $args);
 	}
+});
+
+$app->get('/server/{name}/link', function ($request, $response, $args) {
+	$settings = $this->get('settings')['servers'];
+
+	//check if server in configs
+	if(!array_key_exists($args['name'], $settings)) {
+		$notFoundHandler = $this->get('notFoundHandler');
+		return $notFoundHandler($request, $response);
+	}
+
+	$data['name'] = $settings[$args['name']]['name'];
+	$data['address'] = $settings[$args['name']]['address'];
+	$data['port'] = $settings[$args['name']]['port'];
+
+	return $this->view->render($response, 'server_link.html', $data);
 });
